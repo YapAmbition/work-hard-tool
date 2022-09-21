54c8d4daeee553c01190b26616900cdff812936e
幻影 <zhencheng.szc@alibaba-inc.com>
2022-09-21 15:41:29 +0800

    first commit

---
+++ src/main/java/com/nikfce/wht/work/hard/tool/Application.java
@@ -0,0 +1,17 @@
+package com.nikfce.wht.work.hard.tool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+/**
+ * @author shenzhencheng
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
